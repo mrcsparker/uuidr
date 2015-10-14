20bc0ea893516b9dc84d9e4d66b92c06c8b4d62a
Chris Parker <mrcsparker@gmail.com>
2015-10-14 16:19:05 -0500

    Added documentation

--- src/uuidr.cpp
+++ src/uuidr.cpp
@@ -4,6 +4,11 @@ using namespace Rcpp;
 
 // [[Rcpp::plugins(cpp11)]]
 
+//' @title uuid
+//' @name uuid
+//' @return uuid string
+//' @examples
+//' uuid()
 // [[Rcpp::export]]
 std::string uuid() {
   uuid_t uuid;
@@ -16,6 +21,11 @@ std::string uuid() {
   return std::string(uuid_str);
 }
 
+//' @title uuid_random
+//' @name uuid_random
+//' @return uuid string
+//' @examples
+//' uuid_random()
 // [[Rcpp::export]]
 std::string uuid_random() {
   uuid_t uuid;
@@ -28,6 +38,11 @@ std::string uuid_random() {
   return std::string(uuid_str);
 }
 
+//' @title uuid_time
+//' @name uuid_time
+//' @return uuid string
+//' @examples
+//' uuid_time()
 // [[Rcpp::export]]
 std::string uuid_time() {
   uuid_t uuid;
